0b9032de9db21cecc33fbc2afaf7c1ef631fc0a6
Aureo151 <aureotzic@gmail.com>
2026-05-07 19:32:09 -0600

    agregar funcion que permite agregar espacios y lo asigna a su respectiva tabla

--- frmEspacios.Designer.cs
+++ frmEspacios.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreBuscar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCentro = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(692, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ingrese el nombre unico del espacio a buscar para Modificar o Eliminar";
+            // 
+            // txtNombreBuscar
+            // 
+            this.txtNombreBuscar.Location = new System.Drawing.Point(17, 50);
+            this.txtNombreBuscar.Name = "txtNombreBuscar";
+            this.txtNombreBuscar.Size = new System.Drawing.Size(185, 22);
+            this.txtNombreBuscar.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Centro al Cual Pertenece este Espacio";
+            // 
+            // cmbCentro
+            // 
+            this.cmbCentro.FormattingEnabled = true;
+            this.cmbCentro.Location = new System.Drawing.Point(17, 120);
+            this.cmbCentro.Name = "cmbCentro";
+            this.cmbCentro.Size = new System.Drawing.Size(237, 24);
+            this.cmbCentro.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(17, 178);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(185, 22);
+            this.txtNombre.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 16);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Tipo de Espacio";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Almacenes",
+            "Consultorio",
+            "Laboratorio",
+            "Oficinas",
+            "Sala de Emergencia"});
+            this.cmbTipo.Location = new System.Drawing.Point(17, 233);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(237, 24);
+            this.cmbTipo.TabIndex = 21;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(590, 214);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 25;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(590, 156);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 24;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(590, 101);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 23;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(590, 50);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 22;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 275);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(771, 163);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // frmEspacios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCentro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombreBuscar);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEspacios";
             this.Text = "frmEspacios";
+            this.Load += new System.EventHandler(this.frmEspacios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCentro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- frmEspacios.cs
+++ frmEspacios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ProyectoBDD
 {
@@ -15,6 +16,140 @@ namespace ProyectoBDD
         public frmEspacios()
         {
             InitializeComponent();
+        }
+
+        private void cargarCentrosSalud()
+        {
+            try
+            {
+                Conexion cn = new Conexion();
+
+                using (SqlConnection conexion = cn.ObtenerConexion())
+                {
+                    string sql = "SELECT id_centro, nombre FROM CENTRO_SALUD";
+
+                    using(SqlDataAdapter adapter = new SqlDataAdapter(sql, conexion))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        
+                        cmbCentro.DataSource = null; // Limpiar cualquier enlace previo
+
+                        cmbCentro.DataSource = dataTable;
+                        cmbCentro.DisplayMember = "nombre";
+                        cmbCentro.ValueMember = "id_centro";
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar centros de salud: " + ex.Message);
+            }
+        }
+
+        private void mostrarEspacios()
+        {
+            try
+            {
+                Conexion cn = new Conexion();
+                using (SqlConnection conexion = cn.ObtenerConexion())
+                {
+                    string sql = "SELECT e.id_espacio, e.nombre, c.nombre AS centro FROM ESPACIO e JOIN CENTRO_SALUD c ON e.id_centro = c.id_centro";
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sql, conexion))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar espacios: " + ex.Message);
+            }
+        }
+        private void frmEspacios_Load(object sender, EventArgs e)
+        {
+            cargarCentrosSalud();
+            mostrarEspacios();
+            cmbTipo.SelectedIndex = 0;
+        }
+        private void LimpiarCampos()
+        {
+            txtNombreBuscar.Clear();
+            cmbTipo.SelectedIndex = 0;
+            cmbCentro.SelectedIndex = 0;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexion cn = new Conexion();
+
+                using(SqlConnection conexion = cn.ObtenerConexion())
+                {
+                    conexion.Open();
+
+                    SqlTransaction transaction = conexion.BeginTransaction();
+
+                    try
+                    {
+                        string tipoEspacio = cmbTipo.SelectedItem.ToString();
+
+                        int idCentro = Convert.ToInt32(cmbCentro.SelectedValue);
+
+                        string sqlEspacio = "INSERT INTO ESPACIO (nombre, tipo, " +
+                            "id_centro) VALUES (@nombre, @tipo, @id_centro); " +
+                            "SELECT SCOPE_IDENTITY()";
+
+                        int idEspacioNuevo;
+
+                        using (SqlCommand cmdEspacio = new SqlCommand(sqlEspacio, conexion, transaction))
+                        {
+                            cmdEspacio.Parameters.AddWithValue("@nombre", txtNombreBuscar.Text.Trim());
+                            cmdEspacio.Parameters.AddWithValue("@tipo", tipoEspacio);
+                            cmdEspacio.Parameters.AddWithValue("@id_centro", idCentro);
+                            idEspacioNuevo = Convert.ToInt32(cmdEspacio.ExecuteScalar());
+                        }
+
+                        string tablaTipo = "";
+
+                        if(tipoEspacio == "Almacenes")
+                        tablaTipo = "ALMACENES";
+                        else if(tipoEspacio == "Consultorios")
+                        tablaTipo = "CONSULTORIOS"; 
+                        else if(tipoEspacio == "Laboratorio")
+                        tablaTipo = "LABORATORIO"; 
+                        else if(tipoEspacio =="Oficinas")
+                        tablaTipo = "OFICINAS"; 
+                        else if(tipoEspacio == "Sala de Emergencia")
+                        tablaTipo = "SALA_EMERGENCIA"; 
+
+                        string sqlTipo = $"INSERT INTO {tablaTipo} (id_espacio) VALUES (@id_espacio)";
+
+                        using(SqlCommand cmdTipo = new SqlCommand(sqlTipo, conexion, transaction))
+                        {
+                            cmdTipo.Parameters.Add("@id_espacio", SqlDbType.Int).Value = idEspacioNuevo;
+                            cmdTipo.ExecuteNonQuery();
+                        }
+
+                        transaction.Commit();
+                        MessageBox.Show("Espacio guardado exitosamente.");
+                        mostrarEspacios();
+                        LimpiarCampos();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Error al guardar el espacio: " + ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
